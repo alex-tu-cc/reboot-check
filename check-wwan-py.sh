e757e746edb221832082899273182e03f81ab8e0
Alex Tu <alex.tu@canonical.com>
2016-12-07 15:21:08 +0800

    add check-wwan-py.sh for sniff nm-applet indicator without change it.

---
+++ check-wwan-py.sh
@@ -0,0 +1,50 @@
+#!/bin/sh
+# This script will call a python script to check the items of nm-applet by dbus menu.
+
+MONITOR_STRING="Telecom"
+DBUS_MENU_LOG="/home/u/.cache/dbus-menu-nmapplet.log"
+LOCAL_LOG=/var/local/$0.log
+
+usage() {
+cat << EOF
+usage:  options
+    
+    -h|--help print this message
+    -dry-run tryrun
+    
+EOF
+exit 1
+}
+
+while [ $# -gt 0 ]
+do
+    case "" in
+        -h | --help)
+            usage 0
+            exit 0
+            ;;  
+        --dry-run)
+            DRY_RUN=1;
+            ;;  
+        -s | --string)
+            shift;
+            MONITOR_STRING=$1
+            echo MONITOR_STRING=$MONITOR_STRING
+        ;; 
+        *)
+        usage
+       esac
+       shift
+done
+
+reboot(){
+    echo reboot .... 
+    [ "$DRY_RUN" != "1" ] && systemctl reboot
+    echo reboot .... 
+}
+
+python nm_indicator_dbusmenu_introspect.py&
+echo $(($(cat /var/local/count)+1)) > /var/local/count || true
+sleep 3
+grep $MONITOR_STRING $DBUS_MENU_LOG && reboot
+echo not reboot ....
---
+++ nm_indicator_dbusmenu_introspect.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+from gi.repository import Unity, Gio, GObject, GLib, Dbusmenu
+import os
+
+def on_root_changed(client, data):
+    f= open('/home/u/.cache/dbus-menu-nmapplet.log','a');
+#    print("Client root",client.get_root())
+#    print("Client root children",client.get_root().get_children())
+
+    for child in client.get_root().get_children():
+#        print("  child properties {}".format(child.properties_list()))
+
+        for prop in child.properties_list():
+            print("    {}: {}".format(prop, child.property_get(prop)))
+            f.write("    {}: {}\n".format(prop, child.property_get(prop)))
+    f.close() 
+
+indicators = Gio.bus_get_sync(Gio.BusType.SESSION).call_sync('com.canonical.indicator.application', '/com/canonical/indicator/application/service', 'com.canonical.indicator.application.service', 'GetApplications', None, None, Gio.DBusCallFlags.NONE, -1, None)
+[(menuname, menupath)] = [(i[2], i[3]) for i in indicators[0] if i[8] == 'nm-applet']
+
+print("Dbusmenu.Client.new({}, {})".format(menuname, menupath))
+client=Dbusmenu.Client.new(menuname, menupath);
+client.get_status();
+
+client.connect('root-changed', on_root_changed)
+GLib.MainLoop().run()
