2426f75071030487520c1813995b9e17379544e0
Alex Tu <alex.tu@canonical.com>
2017-06-21 14:07:55 +0800

    reboot stress 30 times.

---
+++ check-custom-stress-reboot.sh
@@ -0,0 +1,58 @@
+#!/bin/sh
+
+# https://bugs.launchpad.net/ubuntu/+source/network-manager/+bug/1647283
+# https://bugs.launchpad.net/somerville/+bug/1639117
+LOCAL_LOG=/var/local/$0.log
+TARGET_CYCLES=30
+usage() {
+cat << EOF
+usage:  options
+    
+    -h|--help print this message
+    -dry-run tryrun
+    
+EOF
+exit 1
+}
+
+while [ $# -gt 0 ]
+do
+    case "" in
+        -h | --help)
+            usage 0
+            exit 0
+            ;;  
+        --dry-run)
+            DRY_RUN=1;
+            ;;  
+        -s | --string)
+            shift;
+            MONITOR_STRING=$1
+            echo MONITOR_STRING=$MONITOR_STRING
+        ;; 
+        *)
+        usage
+       esac
+       shift
+done
+
+reboot(){
+    [ "$DRY_RUN" != "1" ] && systemctl reboot
+}
+
+# if you need to wait networkmanager
+#while [ 1 ]; do
+#    if nmcli d | grep -e "^w"; then
+#        break
+#    else
+#        sleep 5
+#    fi
+#done
+
+echo $(($(cat /var/local/count)+1)) > /var/local/count || true
+if [ $(cat /var/local/count) -gt $TARGET_CYCLES ]; then
+    echo "reached target cycle $TARGET_CYCLES";
+else
+    reboot
+fi
+echo not reboot ....
