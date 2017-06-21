c949abc13bfe4cd44e334ea39fe5c01f0e705010
Alex Tu <alex.tu@canonical.com>
2017-06-21 19:31:46 +0800

    fix bug

--- usr/sbin/check-common.sh
+++ usr/sbin/check-common.sh
@@ -11,7 +11,6 @@ reboot(){
 # $1 is the filter bash command, ex. grep -e "^w"
 # default check interval is 5 secs
 wait_nmcli(){
-    
      #if you need to wait networkmanager
     while [ 1 ]; do
         if nmcli d | eval "$1"; then
@@ -26,10 +25,11 @@ wait_nmcli(){
 # $2 notify string
 wait_and_notify(){
     local sleep_secs=10
-    runs=$1/$sleep_secs
-    for (( i = 0; i < $runs; i++ )); do
+    local i=0
+    runs=$(($1/$sleep_secs))
+    for ((i; i < $runs; i++ )); do
         sleep $sleep_secs
-        notify_user "$2"
+        notify_user "$2 $i in $runs"
     done
 
 
@@ -38,11 +38,11 @@ wait_and_notify(){
 notify_user(){
     [ $# == 0 ] && return
     local xuser_display_pairs=($(who | sed 's/[(|)]/ /g' | awk '{print $1 " " $5}' | grep ":"))
+    local i=0
     for(( i=0; i < ${#xuser_display_pairs[@]}; i+=2));
     do
         XAUTHORITY=/home/${xuser_display_pairs[$i]}/.Xauthority DISPLAY=${xuser_display_pairs[$((i+1))]} notify-send -u critical "$1"
     done
     wall "$1"
-    sleep 5
 }
 
