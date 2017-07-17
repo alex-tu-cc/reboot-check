d60fabf7374b0c01f3c17862169e9799fbec517b
Alex Tu <alex.tu@canonical.com>
2017-07-17 17:25:56 +0800

    reboot: tested

--- usr/sbin/check-custom-reboot-cycle.sh
+++ usr/sbin/check-custom-reboot-cycle.sh
@@ -76,11 +76,11 @@ echo $(($(cat /var/local/count)+1)) > /var/local/count || true
 if [ $(cat /var/local/count) -gt $TARGET_CYCLES ]; then
     echo "reached target cycle $TARGET_CYCLES";
 else
-    if $CHECK_COMMAND ;then
-    ¦   func_update_count_and_reboot
+    if bash -c $CHECK_COMMAND ;then
+        func_update_count_and_reboot
     else
-    ¦   notify_user_interactive "$CHECK_COMMAND failed"
-    ¦   exit 1
+        notify_user_interactive "$CHECK_COMMAND failed"
+        exit 1
     fi
 fi
 echo not reboot ....
